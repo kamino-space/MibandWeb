d0bfbb5626ea8050c25eae120ab281b909efd2fe
kamino <lishichenjs@163.com>
2019-05-04 18:13:34 +0800

    基本功能

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # MibandWeb
-control your Miband2 on website
+control your Miband2 on website  
+forked from [miband-js](https://github.com/vshymanskyy/miband-js/tree/gh-pages)  
+
+## DEMO
+empty
 
 ## RUN
 ```
 npm install yarn -g
 yarn
+# development
 yarn start
+# production
 yarn build
 ```
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
 import MiBand from 'miband';
 import test_all from './test';
-import {Table} from 'antd';
+import {Table, Collapse, Button} from 'antd';
+import $ from 'jquery';
 import 'antd/dist/antd.css';
+import './App.css';
+
+const Panel = Collapse.Panel;
+
+function log() {
+    const msg = [...arguments].join(' ');
+    console.log(msg);
+    $('.connect-log').append(`<p>${msg}</p>`)
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+class NotSupport extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Your Browser Don't Support WebBluetooth</h1>
+                <h2>You can get information at
+                    https://github.com/WebBluetoothCG/web-bluetooth/blob/master/implementation-status.md</h2>
+            </div>
+        )
+    }
+}
 
 class Info extends Component {
     render() {
         const columns = [{
-            title: '参数',
+            title: '名称',
             dataIndex: 'key',
             key: 'key',
         }, {
@@ -16,7 +42,8 @@ class Info extends Component {
             key: 'value',
         }];
         return (
-            <Table dataSource={this.props.dataSource} columns={columns}/>
+            <Table dataSource={this.props.dataSource} loading={this.props.isLoading} columns={columns}
+                   pagination={false}/>
         )
     }
 }
@@ -29,24 +56,27 @@ class App extends Component {
             loading: false,
             support: true,
             connected: false,
+            connecting: false,
             info: [],
+            miband: false,
         };
     }
 
     render() {
         const _this = this;
         const bluetooth = navigator.bluetooth;
-        let miband;
 
         async function connect() {
-            console.log('start connect');
+            log('Start connect');
             if (!bluetooth) {
-                console.log('WebBluetooth is not supported by your browser!');
+                log('WebBluetooth is not supported by your browser!');
                 return;
             }
 
+            _this.setState({connecting: true});
+
             try {
-                console.log('Requesting Bluetooth Device...');
+                log('Requesting Bluetooth Device...');
                 const device = await bluetooth.requestDevice({
                     filters: [
                         {services: [MiBand.advertisementService]}
@@ -55,67 +85,121 @@ class App extends Component {
                 });
 
                 device.addEventListener('gattserverdisconnected', () => {
-                    console.log('Device disconnected');
+                    log('Device disconnected');
                 });
                 await device.gatt.disconnect();
-                console.log('Connecting to the device...');
+                log('Connecting to the device...');
 
                 const server = await device.gatt.connect();
-                console.log('Connected');
+                log('Connected');
 
-                miband = new MiBand(server);
+                log('Connecting to your Miband...');
+                let miband = new MiBand(server);
                 await miband.init();
+                log('Finished. Please wait.');
 
                 _this.setState({connected: true});
 
                 await miband.showNotification('message');
 
+                let info = {
+                    time: await miband.getTime(),
+                    battery: await miband.getBatteryInfo(),
+                    hw_ver: await miband.getHwRevision(),
+                    sw_ver: await miband.getSwRevision(),
+                    serial: await miband.getSerial(),
+                };
+
+                let ped = await miband.getPedometerStats();
+
+                _this.setState({
+                    info: [
+                        {key: '硬件版本', value: info.hw_ver},
+                        {key: '软件版本', value: info.sw_ver},
+                        {key: '电量', value: info.battery.level + '%'},
+                        {key: '时间', value: info.time.toLocaleString()},
+                        {key: '步数', value: ped.steps},
+                        {key: '里程', value: ped.distance + 'm'},
+                        {key: '消耗', value: ped.calories + 'cal'},
+                    ]
+                });
+
+                _this.setState({miband: miband});
+
+                delay(0);
             } catch (error) {
-                console.log('Argh!', error);
+                log('Argh!', error);
             }
+
+            _this.setState({connecting: false})
         }
 
         async function test() {
-            await test_all(miband, console.log)
-        }
-
-        async function get_info() {
-            console.log('start get info');
-            let info = [
-                {key: 'time', value: await miband.getTime()},
-
-            ];
-            console.log(info);
-            _this.setState({info: info});
+            log('ACTION: test all');
+            await test_all(_this.state.miband, log)
         }
 
         async function notice_phone() {
-            await miband.showNotification('phone');
+            log('ACTION: phone notice');
+            await _this.state.miband.showNotification('phone');
+        }
+
+        async function notice_message() {
+            log('ACTION: message notice');
+            await _this.state.miband.showNotification('message');
+        }
+
+        async function notice_close() {
+            log('ACTION: close notice');
+            await _this.state.miband.showNotification('off');
         }
 
         if (this.state.support) {
-            return (
-                <div>
-                    <h1>YES</h1>
-                    <h2>{this.state.connected ? 'CONNECTED' : 'CONNECTING'}</h2>
-                    <button onClick={connect}>
-                        scan
-                    </button>
-                    <button onClick={test}>
-                        test
-                    </button>
-                    <button disabled onClick={get_info}>
-                        info
-                    </button>
-                    <button disabled onClick={notice_phone}>
-                        phone
-                    </button>
-                    {this.state.connected ? <Info dataSource={this.state.info}/> : null}
-                </div>
-            )
+            if (!this.state.connected) {
+                return (
+                    <div className="container">
+                        <h1>MibandWeb</h1>
+                        <h2>点击CONNECT连接到手环</h2>
+                        <div className="connect-line">
+                            <div className="connect-btn" onClick={connect}>
+                                {this.state.connecting ? 'WAIT' : 'CONNECT'}
+                            </div>
+                        </div>
+                        <div className="connect-log">
+                            <h3>connect log</h3>
+                        </div>
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                        <h1>MibandWeb</h1>
+                        <h2>{this.state.connected ? 'CONNECTED' : 'CONNECTING'}</h2>
+                        <Collapse defaultActiveKey={['1']}>
+                            <Panel header="INFO" key="1">
+                                <Info dataSource={this.state.info} isLoading={!this.state.connected}/>
+                            </Panel>
+                            <Panel header="LOG" key="4">
+                                <div className="connect-log" id="log">
+                                    <h3>connect log</h3>
+                                </div>
+                            </Panel>
+                            <Panel header="ACTION" key="2">
+                                <Button block onClick={test}>全部测试</Button><br/>
+                                <Button block onClick={notice_phone}>电话通知</Button><br/>
+                                <Button block onClick={notice_message}>短信通知</Button><br/>
+                                <Button block onClick={notice_close}>关闭通知</Button><br/>
+                            </Panel>
+                            <Panel header="TEST" key="3">
+                                <p>test</p>
+                            </Panel>
+                        </Collapse>
+                    </div>
+                )
+            }
         } else {
             return (
-                <h1>您的浏览器不支持WebBluetooth请使用以下浏览器</h1>
+                <NotSupport/>
             )
         }
     }
