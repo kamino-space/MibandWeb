72f10f682e90c1244387298f227b8b738eb117cf
kamino <lishichenjs@163.com>
2019-05-10 23:08:20 +0800

    更新更新脚本

--- public/update.sh
+++ public/update.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 git init
 git add .
-git commit -m $(date)
+git commit -m '$(date)'
 git branch -m master gh-pages
 git remote add origin git@github.com:kamino-space/MibandWeb.git
 git push origin gh-pages -f
